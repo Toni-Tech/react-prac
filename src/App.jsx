6ed7a0c718e2b7a5ddb80c38f46704e65f6b92c2
Toni <toni@Clarks-iMac.local>
2023-06-15 22:51:26 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-
-
-import './App.css'
+import { useState } from "react";
+import { puppyList } from "./data";
+import "./App.css";
 
 function App() {
- 
-
+  const [featPupId, setFeatPupId] = useState(null);
+  const [puppies, setPuppies] = useState(puppyList);
+  function handleClick() {
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
-      <div>
-
+      <div className="App">
+        {puppies.map((puppy) => {
+          return <p onClick={()=> {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+        })}
+       { featPupId && (<div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+         </div>)}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
